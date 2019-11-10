4d98425d9daaa9b96c50c0501fb9de591f1cf39a
Wojciech Malicki <wojciechmalicki@gmail.com>
2019-11-10 14:47:27 +0100

    add heal properties

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
+# 	
+# 	
+# 	  __ _  __ _ _ __ ___   ___
+# 	 / _` |/ _` | '_ ` _ \ / _ \
+# 	| (_| | (_| | | | | | |  __/
+# 	 \__, |\__,_|_| |_| |_|\___|
+# 	  __/ |
+# 	 |___/
+
 from heroes import *	
 
-Azzo = hero("Azzo", 85, 40, 42)
+Azzo = hero("Azzo", 85, 40, 42, 30)
 Annagab = hero("Annagab", 50, 16, 24)
 Enfor = hero("Enfor", 50, 1, 32)
 Forkt = hero("Forkt", 15, 1, 17)
@@ -11,6 +20,6 @@ Dark = army("Dark")
 Light.add_to_army(Azzo)
 Light.add_to_army(Annagab)
 Dark.add_to_army(Enfor)
-#Dark.add_to_army(Forkt)
+Dark.add_to_army(Forkt)
 
 fight_game(Light, Dark)
--- heroes.py
+++ heroes.py
@@ -12,17 +12,19 @@ from random import randint as rnd
 from itertools import zip_longest as zip_long
 
 class hero():
-    def __init__(self, name, health, mn_attack, mx_attack):
+    def __init__(self, name, health, mn_attack, mx_attack, heal=None):
         self.name = name
-        self.health = health
+        self.cur_health = health
+        self.max_health = health
         self.min_attack = mn_attack
         self.max_attack = mx_attack
         self.defeated = []
         self.sum_of_power = 0
         self.name_of_army = None
+        self.heal = heal
     
     def is_alive(self):
-        if self.health > 0:
+        if self.cur_health > 0:
             return True
         else:
             return False
@@ -31,9 +33,15 @@ class hero():
         return rnd(self.min_attack, self.max_attack) 
 
     def display_stat_full(self):
-        return f"{self.name} [{self.health}:{self.min_attack}-{self.max_attack}]"
+        res = f"{self.name} [{self.cur_health}/{self.max_health}:{self.min_attack}-{self.max_attack}]"
+        if self.heal is None:
+            return res
+        else:
+            return res[:-1] + f":{self.heal}]" 
 
     def display_stat(self, a):
+        """a => True - this is attacker, False - this is defenders
+        """
         if a:
             self.hit_point = self.set_hit_point()
             self.sum_of_power += self.hit_point
@@ -112,7 +120,7 @@ def fight_game(army1, army2):
                 stat2 = n2.display_stat_full()
             else:
                 stat2 = ""
-            print("\t" + stat1 + "\t\t\t" + stat2)
+            print("\t" + stat1 + "\t\t\t\t" + stat2)
 
     def choose():
         pass
